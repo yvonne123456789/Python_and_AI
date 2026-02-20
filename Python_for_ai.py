8679816cf1e24f1010bf11ba6b7d3ff89e52e3f5
yvonne123456789 <85837801+yvonne123456789@users.noreply.github.com>
2026-02-20 03:15:47 +0100

    Add files via upload

---
+++ Python_for_ai.py
@@ -0,0 +1,17 @@
+import requests 
+def get_weather(latitude, longitude):
+    response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m")
+    data = response.json()
+    return data['current']['temperature_2m']
+
+# Get temperature for different cities
+paris_temp = get_weather(48.85, 2.35)
+london_temp = get_weather(51.50, -0.12)
+tokyo_temp = get_weather(35.68, 139.69)
+lusaka_temp = get_weather(-15.387526,28.322817)
+
+print(f"Paris: {paris_temp}°C")
+print(f"London: {london_temp}°C")
+print(f"Tokyo: {tokyo_temp}°C")
+print(f'Lusaka:{lusaka_temp}Celsius')
+  
